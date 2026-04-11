6dc8edab95e088863272d3f9995c702b08d0f068
andree <johelandreevargasvillanueva07@gmail.com>
2026-04-10 23:09:34 -0500

    first commit

---
+++ server/src/main/java/com/andree/synctalk/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.andree.synctalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
