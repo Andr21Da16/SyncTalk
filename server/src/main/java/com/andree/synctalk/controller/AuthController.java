9440535d95cb5fa321ab72a6cb70bf348d2a7b43
andree <johelandreevargasvillanueva07@gmail.com>
2026-04-11 22:06:58 -0500

    feat(user): test

--- server/src/main/java/com/andree/synctalk/controller/AuthController.java
+++ server/src/main/java/com/andree/synctalk/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.andree.synctalk.controller;
 import com.andree.synctalk.dto.Response;
 import com.andree.synctalk.dto.request.auth.RegisterRequest;
 import com.andree.synctalk.service.AuthService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Response<Void>> signUp(@RequestBody RegisterRequest register) {
+    public ResponseEntity<Response<Void>> signUp(@Validated @RequestBody RegisterRequest register) {
         Response<Void> response = authService.registerUser(register);
         return ResponseEntity.ok(response);
     }
